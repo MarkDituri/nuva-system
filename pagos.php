4d43ab46016886dbcabbdf2c5998e9e3b905343a
MarkDituri <marcos.e.dituri@gmail.com>
2021-08-01 05:27:18 -0300

    domingo!
    git status
    exit
    exti()

--- pagos.php
+++ pagos.php
@@ -22,7 +22,7 @@
             <div class="col-xl-10 cont-gral">
                 <div class="row">
                     <div class="col-12 cont-tit">
-                        <h1><a href="miembros.php"><i class="fas fa-credit-card"></i> <b>Pagos</b></a> - Nueva pago</h1> <a class="btn-panel" href="tareas.php"><i class="i-bla fas fa-times-circle"></i> Cerrar</a>
+                        <h1><a href="pagos.php"><i class="fas fa-credit-card"></i> <b>Pagos</b></a></h1> <a class="btn-panel" href="pago-nuevo.php"><i class="i-bla fas fa-plus"></i> Nuevo pago</a>
                     </div>
 
                     <div class="col-12 sinNada cont-dat">
@@ -54,60 +54,79 @@
                     ?>
 
                         <div class="row sinNada sub-tit">
-                            <h1>Nuevo pago</h1>
+                            <h1>Lista de Pago</h1>
                         </div>
+            
+                        <div class="row sinNada">
+                            <div class="col-10 cont-item-pago">
+                                <?php 
+                                    $sql_pagos = "SELECT * FROM pagos order by fecha_pago";
+                                    $sql_pagos_ej = mysqli_query($conexion, $sql_pagos );
 
-                        <form autocomplete="off" action="php/pago-nueva.php"  method="POST">
-                            <div class="row sinNada cont-reg-pass-cl">
-                                <h4>Titulo:*</h4>
-                                <input autocomplete="false" type="text" name="titulo_p" id="titulo_p" class="col-6 input2 b-all"></input>
-                            </div>
-                            <div class="row sinNada cont-reg-pass-cl">
-                                <h4>Detalles:</h4>
-                                <textarea name="detalle_p" id="detalle_p"  rows="3" class="col-6 input2 b-all"></textarea>      
-                            </div>   
-                            <div class="row sinNada cont-reg-pass-cl">
-                                <h4>Asignar a:*</h4>
-                                <select name="asignado_p" id="asignado_p" class="col-6 input2 b-all">
-                                    <option value="0">Elegir cuenta</option>
-                                    <?php                                        
-                                        $lista_usuarios = "SELECT * FROM `usuarios`;";                                                                            
-                                        $lista_usuarios_ej = mysqli_query($conexion, $lista_usuarios);
+                                    while($dato = mysqli_fetch_array($sql_pagos_ej)) {
+                                        $id_pago = $dato['id_pago'];
+                                        $titulo = $dato{'titulo'};         
+                                        $fecha_pago = $dato['fecha_pago'];
+                                        $estado = $dato['estado'];
+                                        $id_usuario = $dato['id_usuario'];
 
-                                        if($lista_usuarios_ej == false){
-                                            echo "No se encontraron miembros";
+                                        if($estado == "Pago"){
+                                            $classEstadoP = "drop-p-pago";
+                                        } else if($estado == "Pendiente"){
+                                            $classEstadoP = "drop-p-pen";
+                                        } else if($estado == "Vencido"){
+                                            $classEstadoP = "drop-p-ven";
                                         }
-                                        else {
-                                            while($dato = mysqli_fetch_array($lista_usuarios_ej)) {
-                                                $id_usuario = $dato['id_usuario'];
-                                                $nombre = $dato['nombre'];
-                                                $apellido = $dato['apellido'];    
-                                                
-                                                echo "<option value='$id_usuario'>$nombre $apellido</option>";
-                                            }
-                                        }
-                                    ?>                     
-                                    
-                                </select>
-                            </div>                     
 
-                            <div class="row sinNada cont-reg-pass-cl">
-                                <h4>Fecha de pago:*</h4>
-                                <input class="col-6 input2 b-all" type="date" name="fecha_pago" id="fecha_pago" value="">
-                                <br>
+                                                     
+                                        $fechaEntera = strtotime($fecha_pago);
+                                        $anio = date("Y", $fechaEntera);
+                                        $mes = date("m", $fechaEntera);
+                                        $dia = date("d", $fechaEntera);
+                                        
+                                        $hora = date("H", $fechaEntera);
+                                        $minutos = date("i", $fechaEntera);
+                                        $segundos = date("s", $fechaEntera);
+
+                                        echo "<div class='row bloque' id='pago-pen'>";
+                                        echo "    <h3 class='col-5'>$titulo</h3>";
+                                        echo "    <div class='col-2 cont-dat-blo'>";
+                                        echo "        <h3><i class='far fa-calendar-alt'></i> $dia-$mes</h3>";
+                                        echo "    </div>";
+                                        echo "    <div class='col-2 cont-dat-blo cont-img-pag'>";
+                                        $sql_usuario = "SELECT * FROM `usuarios` WHERE `id_usuario` = $id_usuario;";
+                                        $sql_usuario_ej = mysqli_query(
+                                            $conexion, $sql_usuario
+                                        );     
+                                        while($dato = mysqli_fetch_array($sql_usuario_ej)) {
+                                            $nombre_res = $dato['nombre'];
+                                            $apellido_res = $dato['apellido'];
+                                            $cargo = $dato['cargo'];
+                                            $img_user = $dato['URL_imagen'];       
+                                            echo "        <div style='background: url(php/$img_user); background-size:cover; background-position: center;' class='img-res'></div>";  
+                                            echo "        <h2>$nombre_res</h2>";
+                                        } 
+                                        echo "    </div>";
+                                        echo "    <div class='col-2 cont-dat-blo'>";
+                                        echo "        <div class='dropdown'>";
+                                        echo "            <a class='$classEstadoP btn btn-secondary dropdown-toggle' href='#' role='button' id='dropdownMenuLink' data-bs-toggle='dropdown' aria-expanded='false'>";
+                                        echo "                <i class='fas fa-check-circle'></i> $estado";
+                                        echo "            </a>";
+                                        echo "            <ul class='dropdown-menu' aria-labelledby='dropdownMenuLink'>";
+                                        echo "                <li><a class='dropdown-item' href='#'>Paga</a></li>";
+                                        echo "                <li><a class='dropdown-item' href='#'>Another action</a></li>";
+                                        echo "                <li><a class='dropdown-item' href='#'>Something else here</a></li>";
+                                        echo "            </ul>";
+                                        echo "        </div>";
+                                        echo "    </div>";
+                                        echo "</div>";                                     
+                                    }
+                                ?>
+                            
+
                             </div>  
+                        </div>                
 
-                            <div class="row sinNada cont-reg-pass-cl">
-                                <h4>Monto $</h4>
-                                <input class="col-6 input2 b-all" type="number" name="monto_p" id="monto_p" value="">
-                                <br>
-                            </div>          
-                                      
-                            <input id="btn-guardar-pago" class="btn-panel ultimo" type="submit" value="Guardar pago">
-                        </form>
-
- 
-                      
                     </div>
 
                 </div>
@@ -116,7 +135,8 @@
 
     <script src="js/jquery-3.3.1.slim.min.js" ></script>
     <script src="js/popper.min.js" ></script>
-    <script src="js/bootstrap.min.js"></script>
+    <!--<script src="js/bootstrap.min.js"></script>-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
--- php/pago-nueva.php
+++ php/pago-nueva.php
@@ -5,7 +5,6 @@ include("conexion.php");
 include("sesion.php");
 
 
-
 $titulo = $_POST['titulo_p'];
 $detalles = $_POST['detalle_p'];
 $id_asignado = $_POST['asignado_p'];
@@ -14,22 +13,20 @@ $monto_p = $_POST['monto_p'];
 
 echo "$titulo<br>$detalles<br>$id_asignado<br>$fecha_pago<br>$monto_p<br>";
 
+// 2. Crear la query (INSERT)
+$consulta = "INSERT INTO `pagos` (`id_pago`, `status`, `estado`, `titulo`, `detalles`, `fecha_pago`, `id_usuario`, `monto`) VALUES 
+(null, 'si','Pendiente','$titulo','$detalles','$fecha_pago','$id_asignado','$monto_p');";
 
+// 3. Ejecutar la query
+$consulta_ej = mysqli_query($conexion, $consulta);
 
-    // 2. Crear la query (INSERT)
-    $consulta = "INSERT INTO `pago`(`id_pago`, `status`, `estado`, `titulo`, `detalle`, `fecha_pago`, `monto`) VALUES 
-    (null, 'si','Pendiente','$titulo','$detalle','$fecha_pago','$monto');";
-    
-    // 3. Ejecutar la query
-    $consulta_ej = mysqli_query($conexion, $consulta);
+if($consulta_ej == false){
+    echo "Fallto ver SQL";
+}
 
-    if($consulta_ej == false){
-        echo "Fallto ver SQL";
-    }
-
-    else {
-        echo "<script>location.href='../pagos.php?ver_dat=si';</script>";
-    }
+else {
+    echo "<script>location.href='../pagos.php?ver_dat=si';</script>";
+}
     
 
 
